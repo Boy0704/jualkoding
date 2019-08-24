8d888443e1bf916f44b6b061c4f7dcededca33a1
Boy Kurniawan <boykurniawan123@gmail.com>
2019-08-25 03:08:10 +0700

    fitur text editor tinycloud di create content aplikasi

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -18,11 +18,35 @@ $sub_menu = strtolower($this->uri->segment(2));
   <script src="assets/admin/js/jquery.dataTables.min.js"></script>
   <script src="assets/admin/js/matrix.js"></script>
   <script src="assets/admin/js/matrix.tables.js"></script>
-  <script src="assets/admin/js/wysihtml5-0.3.0.js"></script>
-  <script src="assets/admin/js/bootstrap-wysihtml5.js"></script>
-  <script>
+  <!-- <script src="assets/admin/js/wysihtml5-0.3.0.js"></script>
+  <script src="assets/admin/js/bootstrap-wysihtml5.js"></script> -->
+  <script src="https://cdn.tiny.cloud/1/4mo39ri6dgnfnyfqwhr6nhicdjgg3nckwd3ruoyr8sa3d5z7/tinymce/5/tinymce.min.js"></script>
+  <!-- <script>
   	$('.textarea_editor').wysihtml5();
-  </script>
+  </script> -->
+  <!-- <script>
+  tinymce.init({
+    selector: '.textarea_editor',  // change this value according to your html
+    plugins: "code image fullscreen media",
+    image_uploadtab: true
+  });
+
+  </script> -->
+  <script type="text/javascript">
+    tinymce.init({
+        selector: ".textarea_editor",
+        plugins: [
+             "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+             "searchreplace wordcount visualblocks visualchars code fullscreen",
+             "insertdatetime nonbreaking save table contextmenu directionality",
+             "emoticons template paste textcolor colorpicker textpattern"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image responsivefilemanager",
+        images_upload_url: '<?php echo base_url() ?>admin/tinymce_upload',
+        images_upload_base_path: '<?php echo base_url() ?>iamges/app_multi',
+        images_upload_credentials: true
+   });
+</script>
 
 <script type="text/javascript">
   // This function is called from the pop-up menus to transfer to
