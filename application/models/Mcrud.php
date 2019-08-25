b1fb242b7326be2d86e782ec608b8506e0279cfb
Boy Kurniawan <boykurniawan123@gmail.com>
2019-08-25 21:27:18 +0700

    Tes Mail Server

--- application/models/Mcrud.php
+++ application/models/Mcrud.php
@@ -75,6 +75,55 @@ class Mcrud extends CI_Model {
 		// $this->email->to("$email");
 		// $this->email->subject($subject);
 		// $this->email->message($pesan);
+
+		$pesan = "
+		<!DOCTYPE html>
+		<html>
+		<head>
+		<style>
+		ol {
+		  background: #ff9999;
+		  padding: 20px;
+		}
+
+		ul {
+		  background: #3399ff;
+		  padding: 20px;
+		}
+
+		ol li {
+		  background: #ffe5e5;
+		  padding: 5px;
+		  margin-left: 35px;
+		}
+
+		ul li {
+		  background: #cce5ff;
+		  margin: 5px;
+		}
+		</style>
+		</head>
+		<body>
+
+		<h1>Styling Lists With Colors:</h1>
+
+		<ol>
+		  <li>Coffee</li>
+		  <li>Tea</li>
+		  <li>Coca Cola</li>
+		</ol>
+
+		<ul>
+		  <li>Coffee</li>
+		  <li>Tea</li>
+		  <li>Coca Cola</li>
+		</ul>
+
+		</body>
+		</html>
+
+		";
+
 		$this->email->from('admin@jualkoding.com', 'Aplikasi Jual Koding');
           $this->email->to('boykurniawan123@gmail.com');
           $this->email->subject('Contoh Kirim Email Dengan Codeigniter');
