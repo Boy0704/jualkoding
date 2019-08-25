2af588166bb066c7def58176a8e9620361656e87
anwarsptr <anwarsptr@gmail.com>
2019-08-25 14:45:34 +0700

    Menambah statistik pengunjung

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,53 @@
 <?php
 $ceks = $this->session->userdata('un_member'); ?>
+
+<?php
+$ip      = $_SERVER['REMOTE_ADDR']; // Dapatkan IP user
+$tanggal = date("Ymd"); // Dapatkan tanggal sekarang
+$waktu   = time(); // Dapatkan nilai waktu
+
+// Cek user yang mengakses berdasarkan IP-nya
+$s = $this->db->query("SELECT * FROM statistik WHERE ip='$ip' AND tanggal='$tanggal'");
+// Kalau belum ada, simpan datanya sebagai user baru
+if($s->num_rows() == 0){
+	$this->db->query("INSERT INTO statistik(ip, tanggal, hits, online) VALUES('$ip', '$tanggal', '1', '$waktu')");
+}
+// Kalau sudah ada, update data hits user
+else{
+	$this->db->query("UPDATE statistik SET hits=hits+1, online='$waktu' WHERE ip='$ip' AND tanggal='$tanggal'");
+}
+
+$query1     = $this->db->query("SELECT * FROM statistik WHERE tanggal='$tanggal' GROUP BY ip");
+$pengunjung = $query1->num_rows();
+
+$query2        = $this->db->query("SELECT COUNT(hits) as total FROM statistik");
+$hasil2        = $query2->row();
+$totpengunjung = $hasil2->total;
+
+$query3 = $this->db->query("SELECT SUM(hits) as jumlah FROM statistik WHERE tanggal='$tanggal' GROUP BY tanggal");
+$hasil3 = $query3->row();
+$hits   = $hasil3->jumlah;
+
+$query4  = $this->db->query("SELECT SUM(hits) as total FROM statistik");
+$hasil4  = $query4->row();
+$tothits = $hasil4->total;
+
+// Cek berapa pengunjung yang sedang online
+$bataswaktu       = time() - 300;
+$pengunjungonline = $this->db->query("SELECT * FROM statistik WHERE online > '$bataswaktu'")->num_rows();
+
+// Angka total pengunjung dalam bentuk gambar
+$folder = "images/counter"; // nama folder
+$ext    = ".png";     // ekstension file gambar
+
+// ubah digit angka menjadi enam digit
+$totpengunjunggbr = sprintf("%06d", $totpengunjung);
+// ganti angka teks dengan angka gambar
+for ($i = 0; $i <= 9; $i++){
+ $totpengunjunggbr = str_replace($i, "<img src=\"$folder/$i$ext\" alt=\"$i\">", $totpengunjunggbr);
+}
+?>
+
 <div class="page-bottom-info">
 		<div class="page-bottom-info-inner">
 
@@ -17,31 +65,42 @@ $ceks = $this->session->userdata('un_member'); ?>
     <div class="container">
       <div class="row">
 
-        <div class=" col-xl-1 col-xl-1 col-md-1 col-6  "></div>
-
-        <div class=" col-xl-2 col-xl-2 col-md-2 col-6  ">
+        <div class=" col-xl-3  ">
           <div class="footer-col">
-            <h4 class="footer-title">Tentang Kami</h4>
-            <ul class="list-unstyled footer-nav">
-              <li><a href="tentang">Tentang Marketplace</a></li>
-            </ul>
+            <h4 class="footer-title" style="margin-bottom:-24px;"><?php echo "$totpengunjunggbr"; ?></h4>
+						 <hr style="margin-bottom:5px;">
+						<?php
+						echo "
+								<img src=\"$folder/hariini.png\"> Pengunjung hari ini : $pengunjung <br>
+								<img src=\"$folder/total.png\"> Total pengunjung    : $totpengunjung <br>
+
+								<img src=\"$folder/hariini.png\"> Hits hari ini  :  $hits <br>
+								<img src=\"$folder/total.png\"> Total hits     :  $tothits <br>
+
+								<img src=\"$folder/online.png\"> Pengunjung Online : $pengunjungonline";
+						 ?>
           </div>
         </div>
 
-        <div class=" col-xl-3 col-xl-3 col-md-3 col-6  ">
+        <div class=" col-xl-3 ">
           <div class="footer-col">
-            <h4 class="footer-title">Bantuan & Kontak Kami</h4>
+            <h4 class="footer-title" style="margin-bottom:-20px;">Bantuan & Kontak Kami</h4>
+						<hr style="margin-bottom:5px;">
             <ul class="list-unstyled footer-nav">
               <li>
-                <a href="kontak">Kontak Marketplace</a>
+                <a href="panduan.html">Panduan</a>
+              </li>
+							<li>
+                <a href="hubungi.html">Kontak</a>
               </li>
             </ul>
           </div>
         </div>
 
-        <div class=" col-xl-2 col-xl-2 col-md-2 col-6  ">
+        <div class=" col-xl-3  ">
           <div class="footer-col">
-            <h4 class="footer-title">Akun</h4>
+            <h4 class="footer-title" style="margin-bottom:-20px;">Akun</h4>
+						<hr style="margin-bottom:5px;">
             <ul class="list-unstyled footer-nav">
               <?php if(isset($ceks)){?>
               <li>
@@ -64,11 +123,12 @@ $ceks = $this->session->userdata('un_member'); ?>
             </ul>
           </div>
         </div>
-        <div class=" col-xl-4 col-xl-4 col-md-4 col-12">
+        <div class=" col-xl-3 ">
           <div class="footer-col row">
             <div class="col-sm-12 col-xs-6 col-xxs-12 no-padding-lg">
               <div class="mobile-app-content">
-                <h4 class="footer-title">Ikuti Kami</h4>
+                <h4 class="footer-title" style="margin-bottom:-20px;">Ikuti Kami</h4>
+								<hr>
                 <ul class="list-unstyled list-inline footer-nav social-list-footer social-list-color footer-nav-inline">
                   <li><a class="icon-color fb" title="Facebook" data-placement="top" data-toggle="tooltip" href="#"><i class="fa fa-facebook"></i> </a></li>
                   <li><a class="icon-color tw" title="Twitter" data-placement="top" data-toggle="tooltip" href="#"><i class="fa fa-twitter"></i> </a></li>
