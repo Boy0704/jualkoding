8ca884efc5978dab3bde2d0a5cb4b426c9f1b797
BOY KURNIAWAN <boykurniawan123@gmail.com>
2019-09-10 09:56:53 +0700

    penambahan statistik user

--- application/views/footer.php
+++ application/views/footer.php
@@ -91,6 +91,21 @@ for ($i = 0; $i <= 9; $i++){ ?>
 				 				<img id='online' src='$folder/n.gif'> <span id='text-pengunjung'>Pengunjung Online : $pengunjungonline</span>";
 				 		 ?>
           </div>
+
+          <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+            <!-- Histats.com  START  (aync)-->
+            <script type="text/javascript">var _Hasync= _Hasync|| [];
+            _Hasync.push(['Histats.start', '1,4308939,4,605,110,55,00011111']);
+            _Hasync.push(['Histats.fasi', '1']);
+            _Hasync.push(['Histats.track_hits', '']);
+            (function() {
+            var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+            hs.src = ('//s10.histats.com/js15_as.js');
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+            })();</script>
+            <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4308939&101" alt="invisible hit counter" border="0"></a></noscript>
+            <!-- Histats.com  END  -->
+          
         </div>
 
         <div class=" col-xl-3 ">
