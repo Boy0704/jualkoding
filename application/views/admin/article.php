9554c9bd8200942ea9d1ad3b5b9df02af56bb2a7
Boy Kurniawan <boykurniawan123@gmail.com>
2020-08-06 16:17:09 +0700

    update lagi

--- application/views/admin/article.php
+++ application/views/admin/article.php
@@ -93,3 +93,23 @@
 
 
 <!--end-main-container-part-->
+
+<script type="text/javascript" src="assets/tinymce/tinymce.min.js"></script>
+    <script type="text/javascript">
+     tinymce.init({
+          selector: ".textarea_editor",
+          plugins: [
+               "advlist autolink link image lists charmap print preview hr anchor pagebreak",
+               "searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
+               "table contextmenu directionality emoticons paste textcolor responsivefilemanager code"
+         ],
+         toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect",
+         toolbar2: "| responsivefilemanager | link unlink anchor | image media | forecolor backcolor  | print preview code ",
+         image_advtab: true ,
+         
+         external_filemanager_path:"assets/filemanager/",
+         filemanager_title:"Responsive Filemanager" ,
+         external_plugins: { "filemanager" : "<?php echo base_url() ?>assets/filemanager/plugin.min.js"}
+     });
+
+</script>
